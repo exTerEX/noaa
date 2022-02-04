09c0fea67337176e722ec45866fc9417ff6704bb
exTerEX <30706568+exTerEX@users.noreply.github.com>
2022-02-05 00:56:19 +0100

    Removed hard coded version in favor of setuptools_scm

--- noaa/__init__.py
+++ noaa/__init__.py
@@ -1,9 +1,1 @@
 from noaa.api import NOAA
-from noaa.api import __author__, __copyright__, __licence__, __version__
-
-__all__ = [
-    "__author__",
-    "__copyright__",
-    "__licence__",
-    "__version__"
-]
